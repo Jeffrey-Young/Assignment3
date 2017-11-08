f96146698f3ecd65cbe3071f18703123936c6d32
Jeffrey-Young <youngjt@live.unc.edu>
2017-11-08 10:32:02 -0500

    more work

--- app/src/main/java/edu/unc/jeffrey/assignment3/MainActivity.java
+++ app/src/main/java/edu/unc/jeffrey/assignment3/MainActivity.java
@@ -3,6 +3,7 @@ package edu.unc.jeffrey.assignment3;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -48,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         db = this.openOrCreateDatabase("Assignment5", Context.MODE_PRIVATE, null);
+        db.execSQL("DROP TABLE IF EXISTS Photos");
+        db.execSQL("DROP TABLE IF EXISTS Tags");
         db.execSQL("CREATE TABLE IF NOT EXISTS Photos (ID INTEGER PRIMARY KEY AUTOINCREMENT, Location VARCHAR(255), Size INTEGER)");
         db.execSQL("CREATE TABLE IF NOT EXISTS Tags (ID INTEGER, Tag VARCHAR(255), FOREIGN KEY (ID) REFERENCES Photos(ID))");
     }
@@ -58,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == PHOTO && resultCode == RESULT_OK) {
             ImageView iv = findViewById(R.id.image);
             photo = (Bitmap) data.getExtras().get("data");
-            final EditText size = (EditText) findViewById(R.id.size);
+            EditText size = (EditText) findViewById(R.id.size);
             size.setText(photo.getByteCount() + "", TextView.BufferType.EDITABLE);
             // save the image
             FileOutputStream out = null;
@@ -89,14 +92,73 @@ public class MainActivity extends AppCompatActivity {
         for (String tag : tags) {
             db.execSQL("INSERT INTO Tags (ID, Tag) VALUES (?, ?)", new String[]{String.valueOf(count), tag});
         }
+        ImageView iv = findViewById(R.id.image);
+        iv.setImageResource(android.R.color.transparent);
+        EditText sizeBox = (EditText) findViewById(R.id.size);
+        sizeBox.setText("");
+        String tagText = "";
+        EditText tagBox = (EditText) findViewById(R.id.tags);
+        tagBox.setText("");
     }
 
     public void load(View view) {
+        EditText stringSize = (EditText) findViewById(R.id.size);
+        int size = 0;
+        if (!(stringSize.getText().toString().equals(""))) {
+            try {
+                size = Integer.parseInt(stringSize.getText().toString());
+            } catch (NumberFormatException e) {
+                // dont care
+            }
+        }
+        String[] tags = ((EditText) findViewById(R.id.tags)).getText().toString().split(";");
+        Cursor c = null;
+        if (tags.length != 0 && size != 0) {
+            c = db.rawQuery("SELECT * FROM Photos, Tags WHERE Photos.ID = Tags.ID AND ? AND Photos.Size = ?", new String[] {convertToString(tags), size + ""});
+        } else if (tags.length == 0) {
+            c = db.rawQuery("SELECT * FROM Photos, Tags WHERE Photos.ID = Tags.ID AND (Photos.Size > ? OR Photos.Size < ?)", new String[] {(size * 0.75) + "", (size * 1.25) + ""});
+
+        } else if (size == 0) {
+            c = db.rawQuery("SELECT * FROM Photos, Tags WHERE Photos.ID = Tags.ID AND ?", new String[] {convertToString(tags)});
+        }
+        // https://stackoverflow.com/questions/2810615/how-to-retrieve-data-from-cursor-class
+        String output = "";
+        if (c.moveToFirst()){
+            do{
+                output += c.getInt(c.getColumnIndex("ID")) + " " + c.getString(c.getColumnIndex("Location")) + " " + c.getString(c.getColumnIndex("Tag")) + " " + c.getInt(c.getColumnIndex("Size")) + "\n";
+            }while(c.moveToNext());
+        }
+        c.close();
+        ImageView iv = findViewById(R.id.image);
+        Log.v("output", output + "");
+        //iv.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/Assignment5/" + output.split("\\n")[0].split(" ")[1]));
+        EditText sizeBox = (EditText) findViewById(R.id.size);
+//        sizeBox.setText(output.split("\\n")[0].split(" ")[3]);
+        String tagText = "";
+//        for (String line : output.split("\\n")) {
+//            tagText += line.split(" ")[2] + ";";
+//        }
+        EditText tagBox = (EditText) findViewById(R.id.tags);
+        tagBox.setText(tagText);
     }
 
     public void capture(View view) {
         count++;
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(cameraIntent, PHOTO);
+    }
+
+    private String convertToString(String[] tags) {
+        String tagText = "";
+        int counter = 0;
+        for (String tag : tags) {
+            if (counter == 0) {
+                tagText += "Tags.Tag = " + tag;
+            } else {
+                tagText += " OR Tags.Tag = " + tag;
+            }
+            counter++;
+        }
+        return tagText;
     }
 }
